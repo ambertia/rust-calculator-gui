aa5092f536c437433875e09981c0e9acb3f44058
ambertia <95402631+ambertia@users.noreply.github.com>
2024-11-13 19:06:32 -0800

    ✨ Lay out top bar of calculator window with CenterBox

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,18 @@ fn build_ui(app: &Application) {
     let box_root = gtk::Box::new(gtk::Orientation::Vertical, 5);
 
     // TODO Calculator input & results rectangle
+    let top_label_result = gtk::Label::builder().hexpand(true).build();
+    let top_label_operation = gtk::Label::builder().width_request(100).build();
+    let top_entry = gtk::Entry::builder().hexpand(true).build();
+
+    // Horizontal box to represent the results pane
+    let box_top = gtk::CenterBox::builder()
+        .vexpand(true)
+        .start_widget(&top_label_result)
+        .center_widget(&top_label_operation)
+        .end_widget(&top_entry)
+        .build();
+    box_root.append(&box_top);
 
     // TODO Calculator buttons
 
