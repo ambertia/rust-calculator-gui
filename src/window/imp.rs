0e9743a81da7e7b082f5afb2283af3781bd91f52
ambertia <95402631+ambertia@users.noreply.github.com>
2024-11-30 17:47:39 -0800

    ✨ Implement calculate()

--- src/window/imp.rs
+++ src/window/imp.rs
@@ -38,13 +38,41 @@ impl Window {
             "." => if !input.contains(".") {
                 self.operand_input_label.set_label(&(input.to_owned() + "."))
             },
-            "=" => self.calculate(),
+            "=" => match self.calculate() {
+                Ok(f) => self.operand_buffer_label.set_label(format!("{f:.5}").as_str()),
+                Err(_) => { self.clear(); self.operand_buffer_label.set_label("ERR"); }
+            },
             _ => {}
         }
     }
 
-    fn calculate(&self) {
+    fn calculate(&self) -> Result<f64, Box<dyn Error>> {
+        // Get the operation label and make sure it's not blank
+        let operation: &str = &self.operation_label.label();
 
+        // Get the first operand from its label
+        let input: f64 = match self.operand_input_label.label().parse() {
+            Ok(f) => f,
+            Err(e) => return Err(Box::new(e)),
+        };
+
+        // Get the second operand from its label, but don't abort on error if the operation doesn't need this operand
+        let buffer: f64 = match self.operand_buffer_label.label().parse() {
+            Ok(f) => f,
+            Err(_) if "√".contains(operation) => 0.,
+            Err(e) => return Err(Box::new(e)),
+        };
+
+        // Conduct calculation
+        return match operation {
+            "+" => Ok(buffer + input),
+            "-" => Ok(buffer - input),
+            "*" => Ok(buffer * input),
+            "÷" => Ok(buffer / input),
+            "^" => Ok(buffer.powf(input)),
+            "√" => Ok(input.sqrt()),
+            _ => Err(Box::<dyn Error>::from("Unknown operation")),
+        }
     }
 }
 
